7a451f40078e821d3923c0ac207f1189fa6fc456
actionchain <actionchain@chainguard.dev>
2026-06-16 20:11:15 +0000

    Update archive/github-actions-slack@v2.11.0

---
+++ src/invoke.js
@@ -0,0 +1,31 @@
+const context = require("./context");
+const { postMessage } = require("./message");
+const { addReaction } = require("./reaction");
+const { updateMessage } = require("./update-message");
+
+const jsonPretty = (data) => JSON.stringify(data, undefined, 2);
+
+const invoke = async () => {
+  try {
+    const func = context.getOptional("slack-function") || "send-message";
+
+    switch (func) {
+      case "send-message":
+        await postMessage();
+        break;
+      case "send-reaction":
+        await addReaction();
+        break;
+      case "update-message":
+        await updateMessage();
+        break;
+      default:
+        context.setFailed("Unhandled `slack-function`: " + func);
+        break;
+    }
+  } catch (error) {
+    context.setFailed("invoke failed:" + error + ":" + jsonPretty(error));
+  }
+};
+
+module.exports = invoke;
